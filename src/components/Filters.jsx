f56a512e61721aca98217e88d96ef89f8c232ad0
Musakalamz <ogunsolumusa@gmail.com>
2024-09-12 17:10:35 +0100

    Filters - Shipping

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -2,7 +2,7 @@ import { Form, useLoaderData, Link } from "react-router-dom";
 import FormInput from "./FormInput";
 import FormSelect from "./FormSelect";
 import FormRange from "./FormRange";
-
+import FormCheckbox from "./FormCheckbox";
 
 function Filters() {
   const { meta } = useLoaderData();
@@ -41,8 +41,11 @@ function Filters() {
         size="select-sm"
       />
 
-{/* PRICE */}
-<FormRange label="select price" name="price" size="range-sm" />
+      {/* PRICE */}
+      <FormRange label="select price" name="price" size="range-sm" />
+
+      {/* SHIPPING */}
+      <FormCheckbox label="free shipping" name="shipping" size="checkbox-sm" />
 
       {/* BUTTONS */}
       <button type="submit" className="btn btn-primary btn-sm ">
