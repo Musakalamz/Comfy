d543d40b8af39acb11a606405440bf6fa2a5fef9
Musakalamz <ogunsolumusa@gmail.com>
2024-09-12 16:48:37 +0100

    Filters - Price range

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,6 +1,8 @@
 import { Form, useLoaderData, Link } from "react-router-dom";
 import FormInput from "./FormInput";
 import FormSelect from "./FormSelect";
+import FormRange from "./FormRange";
+
 
 function Filters() {
   const { meta } = useLoaderData();
@@ -38,6 +40,9 @@ function Filters() {
         list={["a-z", "z-a", "high", "low"]}
         size="select-sm"
       />
+
+{/* PRICE */}
+<FormRange label="select price" name="price" size="range-sm" />
 
       {/* BUTTONS */}
       <button type="submit" className="btn btn-primary btn-sm ">
