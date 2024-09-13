2a7d4fe75a799c0ce3c8c40ba19fdad42f0d4f29
Musakalamz <ogunsolumusa@gmail.com>
2024-09-13 08:27:27 +0100

    Redux set up for cart functionality

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "react-toastify/dist/ReactToastify.css";
+import "./index.css";
+import { Provider } from "react-redux";
+import { store } from "./store.js";
+import { ToastContainer } from "react-toastify";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Provider store={store}>
+      <App />
+      <ToastContainer position="top-center" />
+    </Provider>
+  </StrictMode>
+);
