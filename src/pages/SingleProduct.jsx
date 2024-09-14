505fab979b02aed2f57aaad7a18ca3e2bb7fdb43
Musakalamz <ogunsolumusa@gmail.com>
2024-09-14 15:55:03 +0100

    React Query - Single Product  Page

--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -4,13 +4,29 @@ import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { addItem } from "../features/cart/cartSlice";
 
-export async function loader({ params }) {
-  const response = await customFetch(`/products/${params.id}`);
-  console.log(response);
-  // const product = response.data.data;
-  // console.log(product);
-  return { product: response.data.data };
-}
+// export async function loader({ params }) {
+//   const response = await customFetch(`/products/${params.id}`);
+//   console.log(response);
+//   // const product = response.data.data;
+//   // console.log(product);
+//   return { product: response.data.data };
+// }
+
+const singleProductQuery = (id) => {
+  return {
+    queryKey: ["singleProduct", id],
+    queryFn: () => customFetch.get(`/products/${id}`),
+  };
+};
+
+export const loader =
+  (queryClient) =>
+  async ({ params }) => {
+    const response = await queryClient.ensureQueryData(
+      singleProductQuery(params.id)
+    );
+    return { product: response.data.data };
+  };
 
 function SingleProduct() {
   const { product } = useLoaderData();
